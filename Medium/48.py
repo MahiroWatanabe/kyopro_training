c5a10d46924f2ff6ac043e37b83048ae9b7e2f51
MahiroWatanabe <konchikuwa2@gmail.com>
2023-06-03 23:35:49 +0900

    20230603

--- Medium/48.py
+++ Medium/48.py
@@ -1,18 +1,8 @@
-import sys, re, string
-from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
-from copy import deepcopy, copy
-from collections import Counter, deque, defaultdict
-from heapq import heapify, heappop, heappush
-from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
-from bisect import bisect, bisect_left, bisect_right
-from functools import reduce
-from decimal import Decimal, getcontext
-A = [2,4,6,8]
-cnt = 0
+W,H,x,y = map(int, input().split())
 
-for j in range(1,5):
-    for i in combinations(A,j):
-        print(*i)
-        cnt += 1
+ans1 = 1.0*W*H/2
+ans2 = 0
+if x*2 == W and y*2 == H:
+    ans2 = 1
 
-print(cnt)
+print(ans1,ans2)
--- Medium/49.py
+++ Medium/49.py
@@ -7,13 +7,12 @@ from itertools import accumulate, product, combinations, combinations_with_repla
 from bisect import bisect, bisect_left, bisect_right
 from functools import reduce
 from decimal import Decimal, getcontext
-A = [1,1,1]
-cnt = 0
 
-for j in range(1,3):
-    if j%2 == 0:
-        for i in combinations(A,j):
-            print(*i)
-            cnt += 1
+N,X = map(int, input().split())
+x = list(map(int,input().split()))
+ans = 0
 
-print(cnt)
+for i in x:
+    ans = gcd(ans,abs(X-i))
+
+print(ans)
